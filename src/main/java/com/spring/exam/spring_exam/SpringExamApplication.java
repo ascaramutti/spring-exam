7f934f8101b3c623be21bac452577b834cdf5598
ascaramutti <ascaramutti@hotmail.com>
2024-09-14 23:20:51 -0500

    first configuration

---
+++ src/main/java/com/spring/exam/spring_exam/SpringExamApplication.java
@@ -0,0 +1,13 @@
+package com.spring.exam.spring_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExamApplication.class, args);
+	}
+
+}
